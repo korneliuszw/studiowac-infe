3dd67d6abf019ba00e9c01667b502c288755348d
korneliuszw <korneliuszwstudio@gmail.com>
2023-06-18 22:00:33 +0200

    Show last scrapping + otodom info

--- src/components/Prices/Prices.tsx
+++ src/components/Prices/Prices.tsx
@@ -6,9 +6,14 @@ interface PricesProps {
 
 export async function Prices({city}: PricesProps) {
     const averagePrice = await kv.get<number>(`average_price:rooms:${city}`)
-
+    const lastScrap = await kv.get<string>('last_scrapping')
+    const message = lastScrap ? `Ostatnie sprawdzanie: ${new Intl.DateTimeFormat('pl').format(new Date(lastScrap))}` : ""
     if (!averagePrice) {
         return null
     }
-    return <div className="badge badge-secondary">Średnia cena najmu pokoju: {averagePrice.toFixed(2)}zł</div>
+    return <div className={"tooltip tooltip-bottom"} data-tip={message + ". Pobrano z Otodom"}>
+        <div className="badge badge-secondary">
+            Średnia cena najmu pokoju: {averagePrice.toFixed(2)}zł
+        </div>
+    </div>
 }
